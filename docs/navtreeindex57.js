c4da1f7efe05ece0644079e106a909a830214cc0
bernt-h <81421123+bernt-h@users.noreply.github.com>
2026-07-10 19:12:10 +0000

    Deploying to gh-pages from @ bernt-h/Seamly2D@5f8a6e2fe733858d318ca0a87698e76e0a1bee8b 🚀

--- docs/navtreeindex57.js
+++ docs/navtreeindex57.js
@@ -1,5 +1,13 @@
 var NAVTREEINDEX57 =
 {
+"d3/d5c/classSchemaSymbols.html#a2a31f78138f1cf5437e14930a0ecfc38":[3,0,471,109],
+"d3/d5c/classSchemaSymbols.html#a2b8a64472c7bb35b197a6ab9680e2baf":[3,0,471,168],
+"d3/d5c/classSchemaSymbols.html#a2cbb03d4ebfc61d590e215a433d17c71":[3,0,471,166],
+"d3/d5c/classSchemaSymbols.html#a2dc0d6aabec020d9f83b04c113a2e0ee":[3,0,471,164],
+"d3/d5c/classSchemaSymbols.html#a2efc52f8760af676f020048627c7a55a":[3,0,471,10],
+"d3/d5c/classSchemaSymbols.html#a2f13677c34918db14ae29a25ff33df62":[3,0,471,137],
+"d3/d5c/classSchemaSymbols.html#a2fdb196be3fbf542d6362911135a9076":[3,0,471,74],
+"d3/d5c/classSchemaSymbols.html#a3288f6809ecb29fe1e6238c7fc8ea23f":[3,0,471,16],
 "d3/d5c/classSchemaSymbols.html#a32a1dfc3d340dff9b43bb145b2026eba":[3,0,471,46],
 "d3/d5c/classSchemaSymbols.html#a34e8b1dc50a2624b6fd5f0900116f6b5":[3,0,471,97],
 "d3/d5c/classSchemaSymbols.html#a354ff26183de99d3501afa433d67cfe0":[3,0,471,105],
@@ -123,9 +131,9 @@ var NAVTREEINDEX57 =
 "d3/d5c/classSchemaSymbols.html#ae110b06a30aa63bbac827efda281b071":[3,0,471,34],
 "d3/d5c/classSchemaSymbols.html#ae164c4dfda1a6fa2c532dd300a7fc96c":[3,0,471,167],
 "d3/d5c/classSchemaSymbols.html#ae61f1feac1fdce08bbd35df54beff6ee":[3,0,471,0],
-"d3/d5c/classSchemaSymbols.html#ae61f1feac1fdce08bbd35df54beff6ee":[3,0,471,1],
 "d3/d5c/classSchemaSymbols.html#ae61f1feac1fdce08bbd35df54beff6ee":[3,0,471,3],
 "d3/d5c/classSchemaSymbols.html#ae61f1feac1fdce08bbd35df54beff6ee":[3,0,471,2],
+"d3/d5c/classSchemaSymbols.html#ae61f1feac1fdce08bbd35df54beff6ee":[3,0,471,1],
 "d3/d5c/classSchemaSymbols.html#ae8c28818f55b475a3e61a763f9186eb9":[3,0,471,149],
 "d3/d5c/classSchemaSymbols.html#aecb7513cf63f43d850ca71d3f1e9358b":[3,0,471,72],
 "d3/d5c/classSchemaSymbols.html#aecc9557d6617f9339e00eee335293208":[3,0,471,84],
@@ -241,13 +249,5 @@ var NAVTREEINDEX57 =
 "d3/d63/classMeasurementDatabaseDialog.html#abc04390ac8e8600b37911448e67992bf":[3,0,342,43],
 "d3/d63/classMeasurementDatabaseDialog.html#abe914f0ed745077a9a51335f88124afd":[3,0,342,15],
 "d3/d63/classMeasurementDatabaseDialog.html#ac814fdf6334c1f88acebb335b4055b8d":[3,0,342,27],
-"d3/d63/classMeasurementDatabaseDialog.html#ac88395aa336c2c13af45a404cee3b26e":[3,0,342,6],
-"d3/d63/classMeasurementDatabaseDialog.html#acf2ade9d4b92347d9e4ba731db1b6ee1":[3,0,342,17],
-"d3/d63/classMeasurementDatabaseDialog.html#ad1b28101608110acaa93974286c95901":[3,0,342,59],
-"d3/d63/classMeasurementDatabaseDialog.html#adae8c7ae609a3fd12774540a7660b897":[3,0,342,1],
-"d3/d63/classMeasurementDatabaseDialog.html#adc44ec5341e482ab523f8246159729f8":[3,0,342,24],
-"d3/d63/classMeasurementDatabaseDialog.html#adca59bec905f8bd82817772d5ffbfa2f":[3,0,342,52],
-"d3/d63/classMeasurementDatabaseDialog.html#ae2a5a47c7267e047e970ee564a5142fc":[3,0,342,29],
-"d3/d63/classMeasurementDatabaseDialog.html#ae532a82e691f21b25e1840e6ed8097b2":[3,0,342,20],
-"d3/d63/classMeasurementDatabaseDialog.html#aef8fed2ff6b717dbd061a8c94d21732a":[3,0,342,9]
+"d3/d63/classMeasurementDatabaseDialog.html#ac88395aa336c2c13af45a404cee3b26e":[3,0,342,6]
 };
